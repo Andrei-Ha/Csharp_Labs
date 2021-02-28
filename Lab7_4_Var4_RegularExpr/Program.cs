cb621b12a7f85e427e7629efc7e65fb7c0bb3454
Андрей Сергеич <asgoreglyad@gmail.com>
2021-02-28 19:04:30 +0300

    Final Lab7_4

---
+++ Lab7_4_Var4_RegularExpr/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Lab7_4_Var4_RegularExpr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4.Проект4(по вариантам)
+            //Напишите регулярное выражение и найдите все совпадения в блоке текста.
+            //Вывести все найденные совпадение. 4-вариант. Поиск корректного времени 13:45:35 - корректно 99:66:77 - некорректно
+
+            string data = "45:78:99sd dffds gfds12:12:12 ggggsd789:12:80 fsa23:59:59df25:59:59fds fdfsdf 22:60:45ds.kjhj 23:00:0024:00:00";
+            while (true)
+            {
+                Console.WriteLine("---  Поиск корректного времени в формате HH24:MM:SS  ---");
+                Console.WriteLine("Блок текста:\n" + data);
+                Regex regex = new Regex(@"([01]\d|2[0-3]):[0-5]\d:[0-5]\d");
+                MatchCollection matchCollection = regex.Matches(data);
+                Console.WriteLine("***\nВсе найденные совпадения корректного времени:");
+                foreach (Match m in matchCollection)
+                {
+                    Console.WriteLine(m.Value);
+                }
+                Console.WriteLine("--------------------------------------------------------");
+                Console.WriteLine("Введите строку для добавления в конец имеющегося блока текста (для выхода - 0):");
+                string newstring = Console.ReadLine();
+                if (newstring.Length == 1)
+                {
+                    int quit;
+                    if (Int32.TryParse(newstring, out quit))
+                        if (quit == 0)
+                            return;
+                }
+                data += newstring;
+            }
+        }        
+    }
+}
---
+++ Lab7_4_Var4_RegularExpr/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Lab7_4_Var4_RegularExpr")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab7_4_Var4_RegularExpr")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3ad39b22-5660-4c3e-8f78-75dbb0eb6c33")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
